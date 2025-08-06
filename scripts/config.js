c2a683921ece4351286da62df50326a887ac5354
BryanHui <928102064@qq.com>
2025-08-06 14:09:34 +0800

    chore: add rollup plugin lib

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,13 @@
+const path = require('path')
+const buble = require('rollup-plugin-buble')
+const alias = require('rollup-plugin-alias')
+const cjs = require('rollup-plugin-commonjs')
+const replace = require('rollup-plugin-replace')
+const node = require('rollup-plugin-node-resolve')
+const flow = require('rollup-plugin-flow-no-whitespace')
+
+
+
 const version = process.env.VERSION || require('../package.json').version
 
 const banner = 
